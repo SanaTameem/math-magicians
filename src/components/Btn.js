edf0a80506e044e5b5f49b41c7a751b90a1dea43
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2023-07-14 23:36:52 +0430

    Update  src/components/Btn.js file

--- src/components/Btn.js
+++ src/components/Btn.js
@@ -2,14 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Btn.css';
 
-function Btn({ text }) {
+function Btn({ text, handleInput }) {
   const buttonClass = text === '0' ? 'btn zero-btn' : 'btn';
-  const colorful = ['/', 'x', '+', '-', '='].includes(text) ? 'colorful' : '';
+  const colorful = ['÷', 'x', '+', '-', '='].includes(text) ? 'colorful' : '';
   return (
-    <button type="button" className={`${buttonClass} ${colorful}`}>{text}</button>
+    <button type="button" className={`${buttonClass} ${colorful}`} onClick={handleInput}>{text}</button>
   );
 }
 Btn.propTypes = {
   text: PropTypes.string.isRequired,
+  handleInput: PropTypes.func.isRequired,
 };
 export default Btn;
