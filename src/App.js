8b6d5668760f9cb4379a5dc78805c8366f9bc856
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2023-07-20 18:15:10 +0430

    Update src/App.js file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
 import Calculator from './components/Calculator';
 import Quote from './components/Quote';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
 import './App.css';
 
 function App() {
   return (
     <div className="main">
-      <Calculator />
-      <Quote />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="calculator" element={<Calculator />} />
+        <Route path="quote" element={<Quote />} />
+      </Routes>
     </div>
   );
 }
