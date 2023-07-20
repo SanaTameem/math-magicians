d77480e0d8e3b057655b605a0c23233b7d5f0ed5
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2023-07-20 18:18:37 +0430

    Add  src/components/Navbar.js file

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h1 className="nav-title">Math Magicians</h1>
+      <div className="nav-contianer">
+        <Link to="/" className="nav-item">Home</Link>
+        <Link to="/calculator" className="nav-item bordered">Calculator</Link>
+        <Link to="/quote" className="nav-item">Quote</Link>
+      </div>
+    </nav>
+  );
+}
+export default Navbar;
